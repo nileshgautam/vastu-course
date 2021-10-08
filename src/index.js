d517a0bdad771ab3b491d99f0e313ae42e285bb1
Nilesh_office <nileshwephyre@gmail.com>
2021-10-08 11:14:30 +0530

    disabled dir path

--- src/index.js
+++ src/index.js
@@ -24,24 +24,23 @@ app.use((req, res, next) => {
   next();
 });
 app.use(express.json());
+app.use("/courses", coursesRouter);
+app.use("/admin", adminRouter);
+app.use("/modules", modulesRouter);
+app.use("/lectures", lecturesRouter);
 
-
-if (process.env.NODE_ENV == "production") {
+if ( process.env.NODE_ENV == "production"){
 
   app.use(express.static("coursify/build"));
 
   const path = require("path");
 
-  app.get("*", (req, res) => {
+  // app.get("*", (req, res) => {
 
-    res.sendFile(path.resolve(__dirname, 'coursify', 'build', 'index.html'));
+  //     res.sendFile(path.resolve(__dirname, 'coursify', 'build', 'index.html'));
 
-  })
+  // })
 
-  app.use("/courses", coursesRouter);
-  app.use("/admin", adminRouter);
-  app.use("/modules", modulesRouter);
-  app.use("/lectures", lecturesRouter);
 
 }
 
